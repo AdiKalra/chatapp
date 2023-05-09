491fbf2247f8f52cb0e2c157ac94dd7b80382a6b
AdiKalra <54812001+AdiKalra@users.noreply.github.com>
2023-05-09 14:21:42 +0530

    signup page added

---
+++ client/src/Pages/ChatPage.js
@@ -0,0 +1,16 @@
+import React, { useState, useEffect } from "react";
+// import queryString from "query-string";
+import axios from "axios";
+
+export default function ChatPage(props) {
+  const [data, setData] = useState([]);
+  const fetchChat = async () => {
+    const data = await axios.get("http://localhost:8000/api/chats");
+    setData(data);
+    console.log(data);
+  };
+  useEffect(() => {
+    fetchChat();
+  }, []);
+  return <div>Chat</div>;
+}
