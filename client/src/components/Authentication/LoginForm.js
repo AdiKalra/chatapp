a68367b511f977f368c57779a580ec9d2c512f04
AdiKalra <54812001+AdiKalra@users.noreply.github.com>
2023-05-24 22:42:36 +0530

    signup page connected to backend

--- client/src/components/Authentication/LoginForm.js
+++ client/src/components/Authentication/LoginForm.js
@@ -8,11 +8,60 @@ import {
   VStack,
 } from "@chakra-ui/react";
 import React, { useState } from "react";
+import { useToast } from "@chakra-ui/react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 function LoginForm() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [showPassword, setShowPassword] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const toast = useToast();
+  const navigate = useNavigate();
+  const handleSubmit = async () => {
+    setLoading(true);
+    if (!email || !password) {
+      toast({
+        title: "All fields are requires",
+        status: "warning",
+        duration: 2000,
+        isClosable: true,
+      });
+      setLoading(false);
+    }
+
+    try {
+      const config = {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      };
+
+      const data = {
+        email,
+        password,
+      };
+
+      const { response_data } = await axios.post(
+        "http://localhost:8000/api/user/login",
+        data,
+        config
+      );
+
+      toast({
+        title: "Login Successfull",
+        status: "success",
+        duration: 2000,
+        isClosable: true,
+      });
+      console.log(response_data);
+      localStorage.setItem("login_data", JSON.stringify(response_data));
+      navigate("/api/chat");
+    } catch (err) {
+      console.log(err);
+    }
+  };
   return (
     <VStack>
       <FormControl id="login-email-input" isRequired>
@@ -59,24 +108,28 @@ function LoginForm() {
           color: "#fff",
           bgColor: "#19376D",
         }}
+        isLoading={loading}
+        onClick={() => handleSubmit()}
       >
         Login
-      </Button>
-      <Button
-        mt={4}
-        bgColor="#C74B50"
-        color={"#fff"}
-        type="submit"
-        width={"100%"}
-        _hover={{
-          color: "#fff",
-          bgColor: "#e84B50",
-        }}
-      >
-        Get Guest User Credentials
       </Button>
     </VStack>
   );
 }
 
 export default LoginForm;
+
+
+// <Button
+//   mt={4}
+//   bgColor="#C74B50"
+//   color={"#fff"}
+//   type="submit"
+//   width={"100%"}
+//   _hover={{
+//     color: "#fff",
+//     bgColor: "#e84B50",
+//   }}
+// >
+//   Get Guest User Credentials
+// </Button>;
